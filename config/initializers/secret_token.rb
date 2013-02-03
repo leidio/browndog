efe17f7d70bb36947d38bdaa0e1bf211e9307160
Patrick Jones <ptjones@gmail.com>
2013-02-02 21:53:09 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Browndog::Application.config.secret_token = '755408f3a2e6189755ac774a73a5115779d3815b44a46e701021f3572273070e2d01085f021f5209346d4e790c3aa8d2452df30ebed26ace65d53b870e59823d'
