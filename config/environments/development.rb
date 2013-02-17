1d95669d5fd1068e6962cb10080bd9a1d7fabd68
Patrick Jones <ptjones@gmail.com>
2013-02-16 18:40:35 -0600

    Add User Model with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Browndog::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # In production set host to application host
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
